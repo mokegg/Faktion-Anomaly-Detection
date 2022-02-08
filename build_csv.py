98b2d9e2239458fbc9e9226bb2854a0cb7859dde
mokegg <mekonnen.gebrehiwot1@gmail.com>
2022-02-08 15:44:06 +0100

    Feb 8 working numpy code

--- build_csv.py
+++ build_csv.py
@@ -1,49 +1,32 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Feb  4 10:39:59 2022
-
-@author: mokegg
-"""
-
-
-# Apply compare_image() to folders save results to csv
 import cv2
 import os
 import pandas as pd
-from compare_img import compare_img
+from numpy_compare_Images import compare_img
 
 
-import tensorflow as tf
+#from os.path import splitext
 
-from os.path import splitext
-
-path = 'Data/test_set/00/'
+root = 'Data/test_set/00/'
 result = []
-for filename in os.listdir(path):
-    if filename.endswith('.png'):
-        file_name, ext = splitext(filename)
-        
-        img_ano = tf.image.decode_image(tf.io.read_file(os.path.join(path, filename)))
-       # img_ano = tf.image.rgb_to_grayscale(img_ano)
-        img1 = tf.image.adjust_contrast(img_ano, 0.20)
-        img1 = tf.image.random_brightness(img1, max_delta = 0.1, seed=None)
-        
-        #img1 = cv2.imread(os.path.join(path, filename))
-        df_ssim, Fname1, score1 = compare_img(img1) 
-        print(file_name, Fname1)#compare image with templates
-        result.append([file_name, Fname1, score1])
+         
+for path, subdirs, files in os.walk(root):
+    for file_name in files:
+        if file_name.endswith(('.png', '.jpg')):
+            
+            img1 = cv2.imread(os.path.join(path, file_name),0)
+            ''' The following lines reduce the glare on the images'''
+    
+            blurred = cv2.GaussianBlur(img1, (5,5), 0)
+            thresh = cv2.threshold(blurred, 200, 255, cv2.THRESH_BINARY)[1]
+            dst_TELEA = cv2.inpaint(img1,thresh,3,cv2.INPAINT_TELEA)
+            img1 = dst_TELEA
+            
+            df_ssim, Fname1, score1, msssim_img = compare_img(img1) 
+            print(file_name, Fname1)#compare image with templates
+            result.append([file_name, Fname1, score1, msssim_img])
             
     # =============================================================================
-results = pd.DataFrame(result, columns = ['file_name', 'template', 'score_ssim'])
-results.to_csv('Data/test_set/00/csv.csv', index=False)    
-print(results['score_ssim'].max())
-# =============================================================================
-#result.to_csv(filepath)
-
-# =============================================================================
-# filepath = Path('Data/templates/new/result_ano.csv')  
-# filepath.parent.mkdir(parents=True, exist_ok=True)  
-# result_00.to_csv(filepath) 
-# =============================================================================
+results = pd.DataFrame(result, columns = ['file_name', 'template', 'score_ssim', 'msssim'])
+results.to_csv('Data/test_set/csv_all_test_ano.csv', index=False)    
+print(results['msssim'].min())
 
--- numpy_compare_Images.py
+++ numpy_compare_Images.py
@@ -7,137 +7,80 @@ Created on Thu Feb  3 10:44:14 2022
 """
 
 # =============================================================================
-#import image_similarity_measures
-#from image_similarity_measures.quality_metrics import rmse, ssim, sre
-from sewar.full_ref import mse, rmse, psnr, uqi, ssim, ergas, scc, rase, sam, msssim, vifp
-# import numpy as np
-# from keras.preprocessing.image import load_img
-# import warnings
-# from keras.preprocessing.image import img_to_array
-# from keras.preprocessing.image import array_to_img
-# =============================================================================
+from sewar.full_ref import  ssim, msssim
 import matplotlib.pyplot as plt    
 import cv2
-#import tensorflow as tf
-# =============================================================================
-# img_ano = tf.image.decode_image(tf.io.read_file('Data/train_set/07/16_09_21_07_054.png'))
-# img1 = tf.image.adjust_contrast(img_ano, 0.20)
-# =============================================================================
-
-img_ano = cv2.imread('Data/train_set/ano/17_11_21_anomalies_033.png',0)
-img_ano = cv2.imread('Data/test_set/ano/17_11_21_anomalies_049.png', 0)
-img_ano = cv2.imread('Data/train_set/07/16_09_21_07_039.png',0)
-img_ano = cv2.imread('Data/test_set/05/17_11_21_05_055.png',0)
-
-
 import pandas as pd
 import os
+# =============================================================================
+
+# input files to check the code
+
+#img_ano = cv2.imread('Data/train_set/ano/17_11_21_anomalies_033.png',0)
+img_ano = cv2.imread('Data/test_set/ano/17_11_21_anomalies_001.png', 0)
+#img_ano = cv2.imread('Data/train_set/09/16_09_21_09_014.png',0)
+#img_ano = cv2.imread('Data/test_set/00/17_11_21_00_001.png',0)
+img_ano = cv2.imread('Data/train_set/03/16_09_21_03_094.png',0)
+
+
+''' The following lines reduce the glare on the images'''
+
+blurred = cv2.GaussianBlur(img_ano, (5,5), 0)
+thresh = cv2.threshold(blurred, 200, 255, cv2.THRESH_BINARY)[1]
+dst_TELEA = cv2.inpaint(img_ano,thresh,3,cv2.INPAINT_TELEA)
+img_ano = dst_TELEA
+
+
+
+''' This function takes in an image and compares it to templates to find a similar image; returns the the most 
+similar template and the score of similarity based on SSIM.'''
+
 def compare_img(img_ano):
-    rsmes = []
+
     ssims = []
-    #msssims = []
+
     path = 'Data/templates/'
     for filename in os.listdir(path): #, topdown = True):
         f = os.path.join(path, filename)
         if f.endswith('.png'):
-            # print(f)
             img_template = cv2.imread(f,0) #Load the image
             
-            # RSME
-            rsme = rmse(img_ano.flatten(), img_template.flatten())
-            rsmes.append([f, rsme])
-            
-            #SSIM
+            #CalSSIM
             score, diff = ssim(img_ano, img_template)
             diff = (diff * 255).astype("uint8")
             ssims.append([f, score, diff]) 
-            
-            #MS_SSIM
-            #ms_ssim = msssim(img_ano, img_template)
-            #msssims.append([f, ms_ssim]) 
-            
-    # compare based on RSME (root mean square error)        
-    df_rsme = pd.DataFrame(rsmes, columns = ['fname', 'rsme'])
-    Fname = df_rsme.loc[df_rsme['rsme'].idxmin()].fname
-    rsme1 = df_rsme.loc[df_rsme['rsme'].idxmin()].rsme/100
-    
+                
     # compare cased on SSIM (structuralsimilarity index)
     df_ssim = pd.DataFrame(ssims, columns = ['fname', 'score', diff])
     Fname1 = df_ssim.loc[df_ssim['score'].idxmax()].fname
     score1 = df_ssim.loc[df_ssim['score'].idxmax()].score
-        
-    return df_rsme, df_ssim, Fname, Fname1, rsme1, score1
+    pred_image2 = cv2.imread(Fname1,0)
+    msssim_img=msssim(img_ano, pred_image2, weights=[0.0448, 0.2856, 0.3001, 0.2363, 0.1333], ws=11, K1=0.01, K2=0.03, MAX=None).real
+    return df_ssim, Fname1, score1, msssim_img
 
- #('Data/train_set/00/16_09_21_00_005.png')
-img_template = cv2.imread('Data/templates/template_07.png')
-
-#df_msssim = compare_img(img_ano)
-df_rsme, df_ssim, Fname, Fname1, rsme1, score1 = compare_img(img_ano)
-
-# =============================================================================
-# if rsme1 <= 0.25:
-#    print(f'RSME = {rsme1} : Good')
-# else:
-#     print(f'RMSE = {rsme1} : Anomalous')
-# 
-# pred_image = cv2.imread(Fname)
-# print('Fname: {Fname}')
-# =============================================================================
-
-if score1 >= 0.75:
-   print(f'SSIM ={score1} : Good')
-else:
-    print(f'SSIM ={score1} : Anomalous')
+df_ssim, Fname1, score1, msssim_img = compare_img(img_ano)
        
-pred_image2 = cv2.imread(Fname1)
+pred_image2 = cv2.imread(Fname1,0) # Predicted image
+diff = img_ano - pred_image2 # The difference between the two images
+#diff = cv2.cvtColor(diff,cv2.COLOR_GRAY2RGB)
 
 
-# =============================================================================
-# plt.figure()
-# =============================================================================
+fig, axarr = plt.subplots(3,1) 
 
-#subplot(r,c) provide the no. of rows and columns
-f, axarr = plt.subplots(2,1) 
-
-# use the created array to output your multiple images. In this case I have stacked 4 images vertically
 axarr[0].imshow(img_ano)
 axarr[1].imshow(pred_image2)
-#axarr[2].imshow(img_template1)
+axarr[2].imshow(diff)
 
 #####################################################################################################
 
+# NORMAL OR ANOMALOUS based om MS_SSIM
 
+msssim_img=msssim(img_ano, pred_image2, weights=[0.0448, 0.2856, 0.3001, 0.2363, 0.1333], ws=11, K1=0.01, K2=0.03, MAX=None)
 
+msssin_treshold = 0.9
 
-
-
-
-
-
-
-
-
-
-
-# =============================================================================
-# # Apply compare_image() to folders save results to csv
-# from os.path import splitext
-# path = 'Data/test_set/ano/'
-# result = []
-# for filename in os.listdir(path):
-#     if filename.endswith('.png'):
-#         file_name, ext = splitext(filename)
-#         img = cv2.imread(os.path.join(path, filename))
-#         Fname_rsme, Fname_ssim, score_rsme, score_ssim = compare_img(img) #compare image with templates
-#         result.append([file_name, Fname_rsme, Fname_ssim, score_rsme, score_ssim])
-#         
-# result = pd.DataFrame(result, columns = ['file_name', 'Fname_rsme', 'Fname_ssim', 'score_rsme', 'score_ssim'])
-# 
-# from pathlib import Path  
-# filepath = Path(path +'csv_result.csv')  
-# filepath.parent.mkdir(parents=True, exist_ok=True)  
-# result.to_csv(filepath)
-# result.loc[(result['score_rsme'] > 0.2) & (result['score_ssim'] < 0.8), 'prediction'] = 1 
-# result.loc[(result['score_rsme'] <= 0.2) & (result['score_ssim'] >= 0.8), 'prediction'] = 0
-# result.to_csv(filepath)
-# =============================================================================
+if msssim_img >= msssin_treshold:
+   print(f'MSSSIM: multi-scale structural similarity index = { msssim_img.real} >= { msssin_treshold} : Normal')
+else:
+    print(f'MSSSIM: multi-scale structural similarity index = { msssim_img.real} < {msssin_treshold}: Anomalous')
+       
