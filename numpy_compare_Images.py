bcfcd1c29fed84aa784878dbec154d6448d588ba
mokegg <mekonnen.gebrehiwot1@gmail.com>
2022-02-10 10:27:40 +0100

    code updated

--- numpy_compare_Images.py
+++ numpy_compare_Images.py
@@ -16,16 +16,17 @@ import os
 
 # input files to check the code
 
-#img_ano = cv2.imread('Data/train_set/ano/17_11_21_anomalies_033.png',0)
-img_ano = cv2.imread('Data/test_set/ano/17_11_21_anomalies_001.png', 0)
-#img_ano = cv2.imread('Data/train_set/09/16_09_21_09_014.png',0)
+img_ano = cv2.imread('Data/test_set/ano/17_11_21_anomalies_050.png',0)
+#img_ano = cv2.imread('Data/test_set/ano/17_11_21_anomalies_038.png', 0)
+#img_ano = cv2.imread('Data/test_set/ano/17_11_21_anomalies_031.png',0)
 #img_ano = cv2.imread('Data/test_set/00/17_11_21_00_001.png',0)
-img_ano = cv2.imread('Data/train_set/03/16_09_21_03_094.png',0)
-
+#img_ano = cv2.imread('Data/test_set/03/26_01_22_03_236.png',0)
+#img_ano = cv2.imread('Data/test_set/ano/17_11_21_anomalies_037.png',0)
+#img_ano = cv2.imread('Data/test_set/ano/04.png',0)
 
 ''' The following lines reduce the glare on the images'''
 
-blurred = cv2.GaussianBlur(img_ano, (5,5), 0)
+blurred = cv2.GaussianBlur(img_ano, (7,7), 0)
 thresh = cv2.threshold(blurred, 200, 255, cv2.THRESH_BINARY)[1]
 dst_TELEA = cv2.inpaint(img_ano,thresh,3,cv2.INPAINT_TELEA)
 img_ano = dst_TELEA
@@ -60,16 +61,34 @@ def compare_img(img_ano):
 
 df_ssim, Fname1, score1, msssim_img = compare_img(img_ano)
        
-pred_image2 = cv2.imread(Fname1,0) # Predicted image
-diff = img_ano - pred_image2 # The difference between the two images
+pred_image2 = cv2.imread(Fname1,3) # Predicted image
+img_ano = cv2.cvtColor(img_ano ,cv2.COLOR_GRAY2RGB)
+diff = pred_image2 - img_ano  # The difference between the two images
 #diff = cv2.cvtColor(diff,cv2.COLOR_GRAY2RGB)
 
 
-fig, axarr = plt.subplots(3,1) 
+# NORMAL OR ANOMALOUS based om MS_SSIM
 
-axarr[0].imshow(img_ano)
-axarr[1].imshow(pred_image2)
-axarr[2].imshow(diff)
+msssim_img=msssim(img_ano, pred_image2, weights=[0.0448, 0.2856, 0.3001, 0.2363, 0.1333], ws=11, K1=0.01, K2=0.03, MAX=None)
+
+msssin_treshold = 0.79
+
+if msssim_img >= msssin_treshold:
+   Main_title = f'MS_SSIM = { round(msssim_img.real, 2)} >= { msssin_treshold} : Normal'
+else:
+   Main_title = f'MS_SSIM  = {round(msssim_img.real, 2)} < {msssin_treshold}: Anomalous'
+
+fig, ax = plt.subplots(nrows=3, sharex=True, figsize=(3, 5)) #plt.subplots(3,1) 
+
+ax[0].imshow(img_ano)
+ax[0].set_title('Input image', x=-1.2, y=0.5)
+ax[1].imshow(pred_image2)
+ax[1].set_title('Matching Template', x=-1.2, y=0.35)
+ax[2].imshow(diff)
+ax[2].set_title('Difference', x=-1.2, y=0.25)
+
+fig.suptitle(f'{Main_title}', color="blue",fontsize=16)
+
 
 #####################################################################################################
 
@@ -77,10 +96,11 @@ axarr[2].imshow(diff)
 
 msssim_img=msssim(img_ano, pred_image2, weights=[0.0448, 0.2856, 0.3001, 0.2363, 0.1333], ws=11, K1=0.01, K2=0.03, MAX=None)
 
-msssin_treshold = 0.9
+msssin_treshold = 0.79
 
 if msssim_img >= msssin_treshold:
    print(f'MSSSIM: multi-scale structural similarity index = { msssim_img.real} >= { msssin_treshold} : Normal')
 else:
     print(f'MSSSIM: multi-scale structural similarity index = { msssim_img.real} < {msssin_treshold}: Anomalous')
-       
+ 
+    
